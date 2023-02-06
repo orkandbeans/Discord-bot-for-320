1490b4c68af89cd388a7a48d4fa83a662068b1e2
Gohone8658 <john0893@gmail.com>
2023-02-06 07:57:53 -0800

    add the file for feature1 lorelookup.py

---
+++ src/lorelookup.py
@@ -0,0 +1,4 @@
+#Name: John Gregerson --- Course: CS320 --- Assignment: MainProject Feature 1 --- Date: 02/05/2023
+
+#Initial creation of file for Lore Lookup Feature.
+#Still Pending Design and implementation.
