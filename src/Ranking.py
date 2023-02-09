00158ea33d13305674b87abcec86271eb88daa01
orkandbeans <ltminwa@gmail.com>
2023-02-09 13:06:44 -0800

    Change name and design of classes. add basic methods and comments

--- src/Ranking.py
+++ src/Ranking.py
@@ -6,25 +6,74 @@ class BRIAN():
 
     def __init__(self):
         self.scoreCalculator = ScoreCalculator()
-        self.roleController = RoleController()
+        self.roleController = MemberController()  
+
+        createCommand = "CREATE TABLE IF NOT EXISTS members(member_id INTEGER PRIMARY KEY, member_name TEXT, ranking_score INTEGER)"
+        database.execute(createCommand)
+        database.commit()
 
 
+    def botCommand(self,comType,name,message):
+        #take a bot command and determine what needs to be changed. comType will tell the method what to do.
+        return
+    
+    def updateScore(self,name,message):
+        #change the member "name"'s score based on what they said in their message
+        pass
 
+    def resetRoles(self,name):
+        #reset the roles of the member back to default (given their rankingscore)
+        pass
+
+    def adjustRole(self,name,addRole,role):
+        #change the member "name" to either add or delete "role" based on addRole
+        pass
+
+    def addRemoveMember(self,name,addMember):
+        #remove or add a member from the database based on the variable, "addMember"
+        pass
 
     
+
+
+class MemberController():
+    def __init__(self):
+        self.roleModule = RoleModule()
+        self.memberModule = MemberModule()
+
+    def roleCheck(self,name):
+        #check a member's roles from the db and return a list of roles that the member has
+        return(0)
+
+    def removeMember(self,name):
+        #remove a member from the db based on their name. return 0 on success and 1 on failure
+        return(0)
+    
+    def addMember(self,name):
+        #add a member to the db with member_name = "name". return 0 on success and 1 on failure
+        return(0)
+
+    def addRole(self,name,role):
+        #add a specific role to the member. return 0 on success and 1 on failure
+        return(0)
+
+    def removeRole(self,name,role):
+        #remove a specific role from the member. return 0 on success and 1 on failure
+        return(0)
         
 
-
-class RoleController():
-
-
-
-
-
-    pass
-
-
 class RoleModule():
+    
+    def adjustSpecificRole(self,name,role,addRole):
+        #if addRole is true, add a specific node to a member, else, remove a specific role from a member. 
+        pass
+
+    def getRoles(self,name):
+        #get all roles that member "name" has. return list of roles
+        pass
+
+
+class MemberModule():
 
     def addMember(self, id):
 
@@ -38,12 +87,6 @@ class RoleModule():
         database.execute(command,id)
         database.commit()
 
-    def createDb(self):
-
-        createCommand = "CREATE TABLE IF NOT EXISTS members(member_id INTEGER PRIMARY KEY, member_name TEXT, ranking_score INTEGER)"
-        database.execute(createCommand)
-        database.commit()
-
     def dropDb(self):
 
         dropCommand = "DROP TABLE members"
@@ -54,23 +97,31 @@ class RoleModule():
 
 class ScoreCalculator():
 
+    def __init__(self):
+        self.scoreModule = ScoreModule()
+        self.wordDict = {}
     
-    pass
+    def calculateStr(self,message):
+        #calculate the score of a specific string that a member sent
+        return(0)
+
+    def changeScore(self,name,extra):
+        #increment the member's score by the chat increment amount plus "extra"
+        return(0)
+
 
 class ScoreModule():
     
-    pass
-
-class WordDict():
-
-    pass
+    def adjustScore(self,name,amount):
+        #adjust the score of "name" by "amount" in the database
+        pass
 
 
 
 def main():
 
     
-    brian = RoleModule()
+    brian = BRIAN()
     #brian.dropDb()
     brian.createDb()
     brian.removeMember("jimmy")
