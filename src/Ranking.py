cccbeb5c454af7f5d713c0bb2928bb3eb183637b
orkandbeans <ltminwa@gmail.com>
2023-02-10 12:50:25 -0800

    added the ability to record basic strings as (happy,angry,funny) values and insert scores into the database

--- src/Ranking.py
+++ src/Ranking.py
@@ -8,7 +8,7 @@ class BRIAN():
         self.scoreCalculator = ScoreCalculator()
         self.memberController = MemberController()  
 
-        createCommand = "CREATE TABLE IF NOT EXISTS members(member_id INTEGER PRIMARY KEY, member_name TEXT UNIQUE, ranking_score INTEGER)"
+        createCommand = "CREATE TABLE IF NOT EXISTS members(member_id INTEGER PRIMARY KEY, member_name TEXT UNIQUE, ranking_score INTEGER, happy INTEGER, angry INTEGER, funny INTEGER)"
         database.execute(createCommand)
         database.commit()
 
@@ -19,6 +19,9 @@ class BRIAN():
         if comType == "updateMembers":
             for member in name:
                 self.memberController.addMember(member)
+        
+        if comType == "updateScore":
+            self.scoreCalculator.changeScore(name,self.scoreCalculator.calculateStr(message))
 
         return
     
@@ -82,7 +85,7 @@ class RoleModule():
 class MemberModule():
 
     def addMember(self, name):
-        command = "INSERT OR IGNORE INTO members VALUES ((SELECT max(member_id) FROM members)+1,?,0)"
+        command = "INSERT OR IGNORE INTO members VALUES ((SELECT max(member_id) FROM members)+1,?,0,0,0,0)"
         database.execute(str(command),str(name))
         database.commit()
 
@@ -110,38 +113,39 @@ class ScoreCalculator():
 
     def __init__(self):
         self.scoreModule = ScoreModule()
-        self.wordDict = {}
+        self.wordDict = {"please":(0,5),"hate":(1,5),"LOL":(2,5)}
     
-    def calculateStr(self,message):
-        #calculate the score of a specific string that a member sent
+    def changeScore(self,name,attributeList):
+        #increment the member's score by the chat increment amount plus "extra"
+
+        self.scoreModule.adjustScore(name,attributeList)
+
         return(0)
 
-    def changeScore(self,name,extra):
-        #increment the member's score by the chat increment amount plus "extra"
-        return(0)
+    def calculateStr(self,message):
+        #calculate the score of a specific string that a member sent
+        attributeList = [0,0,0] #this is a list that tracks how nice, mean, and funny someone is. [nice, mean, funny] 
+
+        for word in message.split():
+            if word in self.wordDict:
+                attributeList[self.wordDict[word][0]] += self.wordDict[word][1]
+
+        return(attributeList)
 
 
 class ScoreModule():
     
-    def adjustScore(self,name,amount):
+    def adjustScore(self,name,aL):
         #adjust the score of "name" by "amount" in the database
-        command = "UPDATE members SET ranking_score=?2 WHERE member_name=?1"
-        database.execute(str(command),name,amount)
+        command = "UPDATE members SET ranking_score=ranking_score + 1, happy=happy + ?2, angry=angry + ?3, funny=funny + ?4 WHERE member_name=?1"
+        database.execute(str(command),name,aL[0],aL[1],aL[2])
         database.commit()
         
 
 
 
 def main():
-    sc = ScoreModule()
-    sc.adjustScore('orkandbeans#3110',0)
-    #brian = BRIAN()
-    #MemberModule().dropDb()
-    #brian.createDb()
-    #brian.removeMember("jimmy")
-    #brian.addMember("jimmy")
-    #brian.addMember("jaden")
-   
+    pass
    
 if __name__ == "__main__":
     main()
