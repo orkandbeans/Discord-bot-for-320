328edd30e2a0288a43677d4388d1ee4681dab745
Alshanski <Alshanski0@gmail.com>
2023-02-10 14:26:54 -0800

    Create SoundBoard.py

---
+++ src/SoundBoard.py
@@ -0,0 +1,8 @@
+
+
+if message.content.startswith (':joy:'): #recieve Command
+    #pass the string to SoundBoard class
+    #then that passes the string to the database
+    #retrieve audio
+    #
+    await message.channel.send ('silly_sound'): #place holder
