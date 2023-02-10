30f6555475dfb33c32bd03d9b5153fe91e27218b
JaOnkka <jake.onkka@wsu.edu>
2023-02-09 18:44:32 -0800

    .json file parser

--- src/jeopardy.py
+++ src/jeopardy.py
@@ -1,12 +1,41 @@
 #Jake Onkka --Jeopardy Features --CSE320
+import json
 
-def startgame(command, username, channel):
+import lamda as lamda
+
+
+def startgame(command):#, username, channel):
     command = command.lower()
     if command == 'custom':     #Custom Game Solo: In Progress
-        print(f"{username} from: ({channel})")
+    #    print(f"{username} from: ({channel})")
+        openfile()
         #return str("Hello " + username + " what category do you want?")
         return "Hello"
     if command == 'start':      #Multiplayer Feature: TO-DO
         return 'TBA'
 
     return 'Invalid'
+
+def openfile():
+    with open("JEOPARDY_QUESTIONS1.json", "r") as file:     #THIS IS A LOCAL FILE, IT IS VERY BIG :)
+        data = json.load(file)
+    questions = []          #save questions in here
+    category = "HISTORY"    #select a category
+    for item in data:
+        if item["category"] == category:    #check through every item and match based on category and save it's information
+            value = item["value"]
+            question = item["question"]
+            answer = item["answer"]
+            round = item["round"]
+            show_number = item["show_number"]
+            air_date = item["air_date"]
+        question_data = {
+            "value": value,
+            "question": question,
+            "answer": answer,
+            "round": round,
+            "show_number": show_number,
+            "air_date": air_date
+        }
+        questions.append(question_data)
+    print(questions[0])     #testing
--- src/main.py
+++ src/main.py
@@ -27,16 +27,12 @@ async def on_ready():
 
 # -----------------INSERT BOT COMMANDS HERE--------------------------
 
-@bot.command(name="npclookup")
-async def npclookup(ctx, npc, infotype):
-    output = npcquery(npc, infotype)
-    await ctx.send(output)
 
 @bot.command(name="jeopardy", pass_context=True)
 async def jeopardy(ctx, arg):
-    print(f"{arg.author} ({arg.channel})")
+   # print(f"{arg.author} ({arg.channel})")
     #output = startgame(arg, arg.author, arg.channel)
-
+        startgame("custom")
    # await ctx.send(output)
 
 # -------------------------------------------------------------------
