86b38069cf9bcbae639af73f5b2fe576e5f6e710
Alshanski <Alshanski0@gmail.com>
2023-02-12 12:47:59 -0800

    made changes to SoundBoard.py

--- src/SoundBoard.py
+++ src/SoundBoard.py
@@ -1,70 +1,68 @@
 import discord  # discord API
 import jason    # for database
 
+
 class Soundboard:
-    def constructor(self, command, file):
+    def constructor(self, message):
         # store the command and file in instance variables
-        self.command = command
-        self.file = file
+        self.message = message
+        #self.file = file
 
         # initialize instance of the classes and pass them the required variables
-        self.get Standby(command)
-        self.play = Sound(command)
-        self.secure = Security(file)
-        self.manage = Uploader(command, secure)
+        self.audio = Sound(message)
+        #self.secure = Security(command)
+        #self.manage = Uploader(command, secure)
         
 
-
-class Standby:
-    def constructor(command):
-    # store the command in instance variables
-    self.command = command
-
-    def command_list(self):
-        print(f"")
-
-
 class Sound:
-    def constructor(self, command):
-        self.command = command
+    def constructor(self, message):
+        self.message = message
+        # from the command argument derive the file name
+        self.audio_file = message.content.split(" ")[1] + ".mp3"
+        self.sound = None
+        self.channel = None
 
-    def audio_name(self):
-        # swap code to retrieve an audio from database and play it
-        await message.channel.send(f"playing silly sound")
 
+    async def connect(self):
+        # Join voice channel that the message came from
+        channel = message.author.voice.channel
+        # get the sound object
+        sound = await channel.connect()
 
-
+    async def play(self):
+        # Play the sound file
+        sound.play(discord.FFmpegPCMAudio(audio_file))
+        while sound.is_playing():
+            # finish playing the sound
+            await asyncio.sleep(1)
+        sound.stop()
+        # Disconnect from voice channel
+        await sound.disconnect()
+    
+"""
 class Security:
     def constructor(self, file):
-        #store the file in instance variables
+        # store the file in instance variables
         self.file = file
 
     def check_file_type(self):
 
     def is_it_safe(self):
-
-
-
-class Uploader:
-    def constructor(self, file):
-    #store the command and file in instance variables
-    self.file = file
-    self.command = command
-
-
-
-
-
-
-
+"""
 
 """
-main
+class Uploader:
+    def constructor(self, message, file):
+    # store the command and file in instance variables
+    self.file = file
+    self.message = message
+"""
 
-#Initialize an instance of Soundboard
-    go = Soundboard(command)
-if message.content.startswith (':joy:'): #recieve Command
-    await message.channel
-    go.play.audioname()
-    
+"""main
+# Check for the soundboard slash command
+    #if message.content.startswith('!soundboard upload'):
+    if message.content.startswith('!soundboard'):
+        activate = Soundboard()
+        await activate.audio.connect()
+        await activate.audio.play()
 """
