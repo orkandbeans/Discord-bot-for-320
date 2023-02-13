b8337389e8d4f42ad59ec4ea883c68782886939f
JaOnkka <jake.onkka@wsu.edu>
2023-02-12 17:09:10 -0800

    MultiCategories

--- src/jeopardy.py
+++ src/jeopardy.py
@@ -13,15 +13,38 @@ def answer(answer, arg, value):
         return str(value)
     if arg != answer:
         return str("-" + value)
-def startgame(command):#, username, channel):
+def startgame(command, numberofcategories):#, username, channel):
+
     command = command.lower()
     if command == 'custom':     #Custom Game Solo: In Progress
     #    print(f"{username} from: ({channel})")
-        openfile("HISTORY")
- #       openfile("3-LETTER WORDS")
-    questions = savequestion("HISTORY")
-    return (questions[random.randint(0,5)])
+        with open("jeopardydata.txt", "w") as file: #empty file first
+            myselectcategory = [None] * int(numberofcategories)
+            for i in range(int(numberofcategories)):
+                myrand = random.randint(0, 1000)
+                selectcategory = randomcategory(myrand)
+                savecategory(selectcategory)
+                print(str(selectcategory) + " " + str(i) + " " + str(myrand) + " " + str(numberofcategories))
+                myselectcategory[i] = str(selectcategory)
+                questions = savequestion(selectcategory)
+            #print(myselectcategory)
+        return (myselectcategory)
+        #return (questions[0])
 
+"""
+            data = json.load(file)
+            chosencategories = []
+            chosencategories[0] = data[0]['category']
+            for x in range(len(data)):
+                if data[x]['category'] != chosencategories[x]:
+                    chosencategories[x] = data[x]['category']
+"""
+        #print(chosencategories)
+        #return (questions[0])
+def randomcategory(myrand):
+    with open("JEOPARDY_QUESTIONS1.json", "r") as file:
+        data = json.load(file)
+        return (data[myrand]['category'])
 def savequestion(givencategory):
     with open("JEOPARDY_QUESTIONS1.json", "r") as file:  # THIS IS A LOCAL FILE, IT IS VERY BIG :)
         data = json.load(file)
@@ -48,16 +71,17 @@ def savequestion(givencategory):
             }
             questions.append(question_data)
     return questions
-def openfile(givencategory):
+def savecategory(givencategory):
     questions = savequestion(givencategory)
     # Sort the list of questions by value
-    sorted_questions = sorted(questions, key=lambda x: int(x["value"].replace("$", "").replace(",", ""))) #comparing by strings give wrong value comparison, convert to int
+    sorted_questions = sorted(questions, key=lambda x: int(x["value"].replace("$", "").replace(",", "")) if x["value"] is not None else 0) #comparing by strings give wrong value comparison, convert to int
     ordered_List = []   #order the list by value, least -> greatest
     seen_values = set()
     for q in sorted_questions:
-        if q["value"] not in seen_values:
-            ordered_List.append(q)
-            seen_values.add(q["value"])
-    with open("jeopardydata.txt", "w") as file:     #save the dict into a temp file, this temp file will be used to play the game
-        for x in range(6):
+        if q["value"] is not None and int(q["value"].replace("$", "").replace(",", "")) != 0:
+            if q["value"] not in seen_values:
+                ordered_List.append(q)
+                seen_values.add(q["value"])
+    with open("jeopardydata.txt", "a") as file:     #save the dict into a temp file, this temp file will be used to play the game
+        for x in range(len(ordered_List)):
             file.write(str(ordered_List[x]))
--- src/main.py
+++ src/main.py
@@ -30,16 +30,30 @@ async def on_ready():
 
 @bot.command(name="jeopardy", pass_context=True)
 async def jeopardy(ctx, arg):
+    def check(m):  # only allow the author to answer
+        return m.author == ctx.author
     handle = myhandle(arg)      #user assumed to input "custom" to start a game
     if handle == 'custom':
-        output = startgame(arg)
-        await ctx.send(output['question'])
-        def check(m):       #only allow the author to answer
-            return m.author == ctx.author
-    msg = await bot.wait_for('message', check=check)    #wait for author to type in answer
+        await ctx.send("How many categories would you like to play with? Pick between 1 and 5.")
+        msg = await bot.wait_for('message', check=check)  # wait for author to type in answer
+        while not isinstance(msg.content, int) and 0 > int(msg.content) > 6:
+            await ctx.send("Please enter a valid int")
+            msg = await bot.wait_for('message', check=check)  # wait for author to type in answer
+            if not isinstance(msg.content, int) and 0 < int(msg.content) < 6: break
+        #msg is how many categories to play with
+        message = await ctx.send("``` Choosing categories: ```")
+        output = startgame(arg, msg.content)
+        newmessage = message.content.strip("```")
+        #await ctx.send(msg.content)
+        for i in range(int(msg.content)):
+            await message.edit(content=f"```{output[i]}```")
+        #await ctx.send(output['question'])
 
-    result = answer(output['answer'], msg.content, output['value'])
-    await ctx.send("You got " + result)
+
+    #msg = await bot.wait_for('message', check=check)    #wait for author to type in answer
+
+   # result = answer(output['answer'], msg.content, output['value'])
+   # await ctx.send("You got " + result)
 # -------------------------------------------------------------------
 
 
