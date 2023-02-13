926887cfe6014f7900b7fc36cfd7beaa470bf4ba
tracelogan <100800344+tracelogan@users.noreply.github.com>
2023-02-13 10:36:29 -0800

    Create location_info.py
    Created file containing all 50 states, with longitude and latitude information for obtaining random images

---
+++ src/location_info.py
@@ -0,0 +1,351 @@
+states = [{
+"name": "Alabama",
+"min_latitude": 30.1444444,
+"max_latitude": 35.0080556,
+"min_longitude": -88.4713889,
+"max_longitude": -84.8883333
+},
+{
+"name": "Alaska",
+"min_latitude": 51.1880556,
+"max_latitude": 71.4355556,
+"min_longitude": -179.1422222,
+"max_longitude": -129.9947222
+},
+{
+"name": "Arizona",
+"min_latitude": 31.332,
+"max_latitude": 37.00425,
+"min_longitude": -114.8156389,
+"max_longitude": -109.0451389
+},
+{
+"name": "Arkansas",
+"min_latitude": 33.004,
+"max_latitude": 36.4997222,
+"min_longitude": -94.6170833,
+"max_longitude": -89.6444444
+},
+{
+"name": "California",
+"min_latitude": 32.5288,
+"max_latitude": 42.0095,
+"min_longitude": -124.482003,
+"max_longitude": -114.1309722
+},
+{
+"name": "Colorado",
+"min_latitude": 36.9924,
+"max_latitude": 41.0039,
+"min_longitude": -109.060250,
+"max_longitude": -102.0415
+},
+{
+"name": "Connecticut",
+"min_latitude": 40.9585,
+"max_latitude": 42.0505,
+"min_longitude": -73.727722,
+"max_longitude": -71.787222
+},
+{
+"name": "Delaware",
+"min_latitude": 38.4510,
+"max_latitude": 39.8394444,
+"min_longitude": -75.7890278,
+"max_longitude": -75.0489167
+},
+{
+"name": "Florida",
+"min_latitude": 24.5210,
+"max_latitude": 31.000,
+"min_longitude": -87.634,
+"max_longitude": -79.9743333
+},
+{
+"name": "Georgia",
+"min_latitude": 30.35,
+"max_latitude": 35.0004167,
+"min_longitude": -85.6051667,
+"max_longitude": -80.84
+},
+{
+"name": "Hawaii",
+"min_latitude": 18.91083,
+"max_latitude": 28.4175,
+"min_longitude": -178.341667,
+"max_longitude": -154.808889
+},
+{
+"name": "Idaho",
+"min_latitude": 41.988056,
+"max_latitude": 49.000278,
+"min_longitude": -117.243056,
+"max_longitude": -111.0435
+},
+{
+"name": "Illinois",
+"min_latitude": 36.970278,
+"max_latitude": 42.508333,
+"min_longitude": -91.513056,
+"max_longitude": -87.019722
+},
+{
+"name": "Indiana",
+"min_latitude": 37.7717,
+"max_latitude": 41.7613889,
+"min_longitude": -88.0972222,
+"max_longitude": -84.7844444
+},
+{
+"name": "Iowa",
+"min_latitude": 40.3755,
+"max_latitude": 43.501389,
+"min_longitude": -96.6395833,
+"max_longitude": -90.14
+},
+{
+"name": "Kansas",
+"min_latitude": 36.988,
+"max_latitude": 40.003,
+"min_longitude": -102.0517,
+"max_longitude": -94.5886667
+},
+{
+"name": "Kentucky",
+"min_latitude": 36.497,
+"max_latitude": 39.1472222,
+"min_longitude": -89.5715833,
+"max_longitude": -81.9649167
+},
+{
+"name": "Louisiana",
+"min_latitude": 28.904167,
+"max_latitude": 33.019167,
+"min_longitude": -94.0430556,
+"max_longitude": -88.8172222
+},
+{
+"name": "Maine",
+"min_latitude": 43.068,
+"max_latitude": 47.458056,
+"min_longitude": -71.0830556,
+"max_longitude": -66.9498333
+},
+{
+"name": "Maryland",
+"min_latitude": 37.910083,
+"max_latitude": 39.723083,
+"min_longitude": -79.4876583,
+"max_longitude": -75.0489167
+},
+{
+"name": "Massachusetts",
+"min_latitude": 41.187083,
+"max_latitude": 42.886667,
+"min_longitude": -73.508,
+"max_longitude": -69.86
+},
+{
+"name": "Michigan",
+"min_latitude": 41.696111,
+"max_latitude": 48.2394,
+"min_longitude": -90.4180556,
+"max_longitude": -82.122778
+},
+{
+"name": "Minnesota",
+"min_latitude": 43.499361,
+"max_latitude": 49.384359,
+"min_longitude": -97.23925,
+"max_longitude": -89.4833333
+},
+{
+"name": "Mississippi",
+"min_latitude": 28.7,
+"max_latitude": 34.9958333,
+"min_longitude": -91.655,
+"max_longitude": -88.0972222
+},
+{
+"name": "Missouri",
+"min_latitude": 35.995,
+"max_latitude": 40.613889,
+"min_longitude": -96.6397222,
+"max_longitude": -89.0988889
+},
+{
+"name": "Montana",
+"min_latitude": 44.358333,
+"max_latitude": 49.001389,
+"min_longitude": -116.050003,
+"max_longitude": -104.039167
+},
+{
+"name": "Nebraska",
+"min_latitude": 39.9999,
+"max_latitude": 43.001722,
+"min_longitude": -104.0535,
+"max_longitude": -95.3088889
+},
+{
+"name": "Nevada",
+"min_latitude": 35.0,
+"max_latitude": 42.001944,
+"min_longitude": -120.005833,
+"max_longitude": -114.039167
+},
+{
+"name": "New Hampshire",
+"min_latitude": 42.696111,
+"max_latitude": 45.305278,
+"min_longitude": -72.5572222,
+"max_longitude": -70.6608333
+},
+{
+"name": "New Jersey",
+"min_latitude": 38.9279167,
+"max_latitude": 41.358056,
+"min_longitude": -75.558611,
+"max_longitude": -74.35
+},
+{
+"name": "New Mexico",
+"min_latitude": 31.331944,
+"max_latitude": 37.000278,
+"min_longitude": -109.050278,
+"max_longitude": -103.000278
+},
+{
+"name": "New York",
+"min_latitude": 40.471944,
+"max_latitude": 45.015833,
+"min_longitude": -79.7625,
+"max_longitude": -71.7772222
+},
+{
+"name": "North Carolina",
+"min_latitude": 33.7528,
+"max_latitude": 36.588333,
+"min_longitude": -84.321944,
+"max_longitude": -75.400833
+},
+{
+"name": "North Dakota",
+"min_latitude": 45.9350,
+"max_latitude": 49.000278,
+"min_longitude": -104.048972,
+"max_longitude": -96.554444
+},
+{
+"name": "Ohio",
+"min_latitude": 38.403889,
+"max_latitude": 42.327222,
+"min_longitude": -84.820278,
+"max_longitude": -80.518611
+},
+{
+"name": "Oklahoma",
+"min_latitude": 33.6267,
+"max_latitude": 37.001667,
+"min_longitude": -103.004722,
+"max_longitude": -94.439167
+},
+{
+"name": "Oregon",
+"min_latitude": 41.9917,
+"max_latitude": 46.294722,
+"min_longitude": -124.566389,
+"max_longitude": -116.463889
+},
+{
+"name": "Pennsylvania",
+"min_latitude": 39.719722,
+"max_latitude": 42.269722,
+"min_longitude": -80.520278,
+"max_longitude": -74.688611
+},
+{
+"name": "Rhode Island",
+"min_latitude": 41.146389,
+"max_latitude": 42.018611,
+"min_longitude": -71.862222,
+"max_longitude": -71.120556
+},
+{
+"name": "South Carolina",
+"min_latitude": 32.0348,
+"max_latitude": 35.215,
+"min_longitude": -83.353889,
+"max_longitude": -78.55
+},
+{
+"name": "South Dakota",
+"min_latitude": 42.475,
+"max_latitude": 45.945278,
+"min_longitude": -104.057222,
+"max_longitude": -96.436389
+},
+{
+"name": "Tennessee",
+"min_latitude": 34.98,
+"max_latitude": 36.678333,
+"min_longitude": -90.310278,
+"max_longitude": -81.646667
+},
+{
+"name": "Texas",
+"min_latitude": 25.83,
+"max_latitude": 36.500556,
+"min_longitude": -106.645,
+"max_longitude": -93.508611
+},
+{
+"name": "Utah",
+"min_latitude": 36.9979,
+"max_latitude": 42.001389,
+"min_longitude": -114.05,
+"max_longitude": -109.041389
+},
+{
+"name": "Vermont",
+"min_latitude": 42.726944,
+"max_latitude": 45.016667,
+"min_longitude": -73.437778,
+"max_longitude": -71.465278
+},
+{
+"name": "Virginia",
+"min_latitude": 36.540833,
+"max_latitude": 39.466389,
+"min_longitude": -83.675556,
+"max_longitude": -75.24
+},
+{
+"name": "Washington",
+"min_latitude": 45.5425,
+"max_latitude": 49.001389,
+"min_longitude": -124.783,
+"max_longitude": -116.915833
+},
+{
+"name": "West Virginia",
+"min_latitude": 37.204444,
+"max_latitude": 40.638611,
+"min_longitude": -82.643333,
+"max_longitude": -77.719167
+},
+{
+"name": "Wisconsin",
+"min_latitude": 42.491944,
+"max_latitude": 47.309722,
+"min_longitude": -92.888333,
+"max_longitude": -86.245556
+},
+{
+"name": "Wyoming",
+"min_latitude": 40.994722,
+"max_latitude": 45.005833,
+"min_longitude": -111.0525,
+"max_longitude": -104.051667
+}
+]
