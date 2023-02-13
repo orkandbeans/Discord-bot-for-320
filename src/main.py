a5195fa602eeb8d57185be94de681bf65efdc203
Aaron Laferty <ViscountGames101@gmail.com>
2023-02-12 18:37:03 -0800

    Created Dalle Feature and starting chatGPT
    I finished my Dalle Feature for the most part and will be working on chatGPT and databasing next

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+from openAI import openAI
 
 import random
 import os
@@ -8,7 +10,15 @@ import os
 #Create bot declaration with intents
 bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
 
+<<<<<<< Updated upstream
 #when bot is logged in
+=======
+# Create bot declaration with intents
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+AI = openAI()
+
+# when bot is logged in
+>>>>>>> Stashed changes
 @bot.event
 async def on_ready():
     print("Bot is Up and Ready")
@@ -21,8 +31,41 @@ async def on_ready():
     except Exception as e:
         print(e)
 
+<<<<<<< Updated upstream
 """
 -----------------INSERT BOT COMMANDS HERE--------------------------
+=======
+
+# -----------------INSERT BOT COMMANDS HERE--------------------------
+@bot.tree.command(name="aisetup", description = "Start setup process of openAI API for arnold bot for the server")
+async def aisetup(ctx: discord.Interaction):
+    await ctx.response.send_message("Okay! Creating a DM!")
+    dm = await ctx.user.create_dm()
+    author = ctx.user
+    server = ctx.guild.name
+    await dm.send(f"Hello {author}! You just used the setup command for openAI on the server {server}! Please provide the openAI API key to your wallet in order to make openAI commands available. you must provide the"
+     + " API key in its entirety in this message. It will then be linked to this server.")
+    APIkey = await bot.wait_for('message')
+    # AI.insertKey(APIkey.content, ctx.guild.id)
+    await dm.send("GOOD WE GOT THE KEY :)")
+
+@bot.tree.command(name = "dalle", description = "Give a prompt and let openAI generate an image")
+@app_commands.describe(prompt = "What prompt would you like to generate?")
+async def dalle(ctx: discord.Interaction, prompt: str):
+    await ctx.response.send_message("Give us a few seconds to generate your image.")
+    user = ctx.user.id;
+    data = AI.dalle.generate(prompt)
+    embed = discord.Embed()
+    embed.set_image(url=data)
+    await ctx.channel.send(f"<@{user}> here's your image for prompt: {prompt}", embed=embed)
+
+
+
+@bot.command(name="npclookup")
+async def npclookup(ctx, npc, infotype):
+    output = npcquery(npc, infotype)
+    await ctx.send(output)
+>>>>>>> Stashed changes
 
 
 
---
+++ src/openAI.py
@@ -0,0 +1,18 @@
+#from db import db
+from dalle import Dalle
+from dotenv import load_dotenv
+#from chatGPT import chatGPT
+import os
+
+class openAI:
+    def __init__(self):
+        #self.APIkey = self.fetchAPI(serverID)
+        load_dotenv()
+        APIKEY = os.getenv('API_TOKEN')
+        self.APIkey = APIKEY
+        self.dalle = Dalle(self.APIkey)
+        #self.chatGPT = chatGPT(self.APIkey)
+
+    #def fetchAPI(self, serverID):
+
+    #def insertKey(self, APIkey, serverID)
