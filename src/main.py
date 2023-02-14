810f5856c3aaee2826dbdd3a9a9fd56cb5b5670a
JaOnkka <jake.onkka@wsu.edu>
2023-02-13 23:10:19 -0800

    Reverting main back to just my command for testing my code only

--- src/main.py
+++ src/main.py
@@ -4,22 +4,14 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
-import Ranking
-from openAI import openAI
-import random
+
 import os
-from lorelookup import *
-from osrsinfo import *
 
-
-#Create bot declaration with intents
 
 
 
 bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
-#Create BRIAN declaration for ranking
-Brian = Ranking.BRIAN()
-AI = openAI()
+
 
 
 #when bot is logged in
@@ -36,10 +28,6 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready():
     print("Bot is Up and Ready")
 
-    #get the guild with all users in our specific discord and run an update on the members of the database
-    memberList = bot.get_guild(1065019755628613682).members
-    Brian.botCommand("updateMembers",memberList,"")
-
     #try to sync all commands that aren't actively in the tree or have been altered
     try:
         synced = await bot.tree.sync()
@@ -50,49 +38,6 @@ async def on_ready():
 
 
 # -----------------INSERT BOT COMMANDS HERE--------------------------
-@bot.tree.command(name="osrsinfo")
-async def osrs_info_command(interaction: discord.Interaction, entity_name: str, search_option: int = 0):
-    command_output = osrsinfo(entity_name, search_option)
-    await interaction.response.send_message(command_output[0])
-    for i in range(1, len(command_output)):
-        await interaction.followup.send(command_output[i])
-
-@bot.tree.command(name="aisetup", description = "Start setup process of openAI API for arnold bot for the server")
-async def aisetup(ctx: discord.Interaction):
-    await ctx.response.send_message("Okay! Creating a DM!")
-    dm = await ctx.user.create_dm()
-    author = ctx.user
-    server = ctx.guild.name
-    await dm.send(f"Hello {author}! You just used the setup command for openAI on the server {server}! Please provide the openAI API key to your wallet in order to make openAI commands available. you must provide the"
-     + " API key in its entirety in this message. It will then be linked to this server.")
-    APIkey = await bot.wait_for('message')
-    # AI.insertKey(APIkey.content, ctx.guild.id)
-    await dm.send("GOOD WE GOT THE KEY :)")
-
-@bot.tree.command(name = "dalle", description = "Give a prompt and let openAI generate an image")
-@app_commands.describe(prompt = "What prompt would you like to generate?")
-async def dalle(ctx: discord.Interaction, prompt: str):
-    await ctx.response.send_message("Give us a few seconds to generate your image.")
-    user = ctx.user.id
-    data = AI.dalle.generate(prompt)
-    embed = discord.Embed()
-    embed.set_image(url=data)
-    await ctx.channel.send(f"<@{user}> here's your image for prompt: {prompt}", embed=embed)
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    Brian.updateScore(message.author,message.content)
-
-@bot.event
-async def on_member_ban(guild, member):
-    Brian.addRemoveMember(member,False)
-
-@bot.event
-async def on_member_join(member):
-    Brian.addRemoveMember(member,True)
-
 @bot.command(name="jeopardy", pass_context=True)
 async def jeopardy(ctx, arg):
     money = 0
